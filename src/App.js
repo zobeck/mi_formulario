726f442d7381091c5f3a72d17108b6d1ca877df9
zobeck <ada_hack38@hackademy.mx>
2023-03-15 02:32:27 -0600

    Enviar una imagen base64

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import {useState} from "react";
 
 function App() {
+  const [image, setImage] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+
+
+  //Hacemos la conexión con la API
+  //En este ejemp´lo estamos usando Axios
+  //Instala axios ejecutando npm i axios en tu consola, asegurate de estar dentro de tu carpeta
+  //del proyecto
+ // Esta constante maneja el envio de la img al EP
+ //Tambien pueden nombrarla hamdleSubmit
+  const enviarInfo = (e) =>{
+    e.preventDefault();
+    setError('');
+
+    if (image.trim() === '') {
+      setError('archivo invalido');
+      return(
+          <>
+          <div> 
+              Por favor elige una imágen
+          </div>
+          </>
+      );
+  }
+  const bodyParameters = {
+    urlImagen: image
+};
+
+axios.post(`https://mi-primer-formulario-b9dcc-default-rtdb.firebaseio.com/imagen`, 
+bodyParameters,
+)
+.then((response) => {
+    console.log(response);
+    if(response.statusText === "OK"){
+        setSuccess(true);
+    }            
+});
+
+
+  }
+
+  //Definimos el evento en el que se va a subir la imágen
+  //el console.log lo pueden eliminar despúes es solo
+  //para poder checar en la consola que todo esta fincionando
+  const subirImagen = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await convertB64(file);
+    console.log(base64);
+  };
+
+  // Aquí es dónde transformamos el archivo
+  // a base64
+  const convertB64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+   
+   
+
+  };
+
+  //Definimos el tipo de input como file
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <form>
+          <p>Elige la imagen que quieres subir</p>
+          <input
+            type="file"
+            value={image}
+            onClick= {(e) =>
+              setImage(e.currentTarget.value)}
+            onChange={(e) => {
+              subirImagen(e);}
+           
+        
+            
+             // => setLink(event.currentTarget.value)}
+               // placeholder="Ingresa el link de tu video"/
+          }
+          ></input>
+
+          <br></br>
+             <button
+             className="boton"
+             onClick={enviarInfo}
+             >
+               Guardar imagen
+               
+             </button>
+        
+        </form>
+      </main>
     </div>
   );
 }
