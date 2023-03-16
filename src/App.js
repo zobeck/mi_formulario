7f638c88b5aacc925cd06e341a34881def11824f
zobeck <ada_hack38@hackademy.mx>
2023-03-15 23:35:11 -0600

    Img  base 64

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
 import "./App.css";
-import axios from "axios";
-import {useState} from "react";
-
-function App() {
-  const [image, setImage] = useState('');
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState(false);
+//import axios from "axios";
+//import {useState} from "react";
 
 
-  //Hacemos la conexión con la API
-  //En este ejemp´lo estamos usando Axios
-  //Instala axios ejecutando npm i axios en tu consola, asegurate de estar dentro de tu carpeta
-  //del proyecto
- // Esta constante maneja el envio de la img al EP
- //Tambien pueden nombrarla hamdleSubmit
-  const enviarInfo = (e) =>{
-    e.preventDefault();
-    setError('');
 
-    if (image.trim() === '') {
-      setError('archivo invalido');
-      return(
-          <>
-          <div> 
-              Por favor elige una imágen
-          </div>
-          </>
-      );
-  }
-  const bodyParameters = {
-    urlImagen: image
-};
-
-axios.post(`https://mi-primer-formulario-b9dcc-default-rtdb.firebaseio.com/imagen`, 
-bodyParameters,
-)
-.then((response) => {
-    console.log(response);
-    if(response.statusText === "OK"){
-        setSuccess(true);
-    }            
-});
-
-
-  }
+function App(){
 
   //Definimos el evento en el que se va a subir la imágen
   //el console.log lo pueden eliminar despúes es solo
@@ -68,10 +29,6 @@ bodyParameters,
         reject(error);
       };
     });
-
-   
-   
-
   };
 
   //Definimos el tipo de input como file
@@ -82,23 +39,16 @@ bodyParameters,
           <p>Elige la imagen que quieres subir</p>
           <input
             type="file"
-            value={image}
-            onClick= {(e) =>
-              setImage(e.currentTarget.value)}
             onChange={(e) => {
               subirImagen(e);}
-           
-        
-            
-             // => setLink(event.currentTarget.value)}
-               // placeholder="Ingresa el link de tu video"/
+ 
           }
           ></input>
 
           <br></br>
              <button
              className="boton"
-             onClick={enviarInfo}
+             //onClick={enviarInfo}
              >
                Guardar imagen
                
